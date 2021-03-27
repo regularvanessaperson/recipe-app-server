eb684d2769258d307e5c97743477a6ebba4e272b
regularvanessaperson <vamavila4@gmail.com>
2021-03-27 10:06:53 -0500

    can get all recipes related to user

--- routes/recipe.js
+++ routes/recipe.js
@@ -8,11 +8,12 @@ const authorization = require("../middleware/authorization");
 //retrieve all recipes
 router.get("/", authorization, async(req, res)=>{
     try{
-        const recipe = await pool.query("SELECT * FROM recipes WHERE fk_user_id =$1", [
+        
+        const recipe = await pool.query("SELECT * FROM recipes WHERE user_id =$1", [
             req.user
         ])
-        console.log(recipe)
-        // res.json(recipe);
+
+        res.json(recipe.rows);
     }catch( err) {
         console.error(err.message)
         res.status(500).json("Error when looking for all recipes")
@@ -23,12 +24,7 @@ router.get("/", authorization, async(req, res)=>{
 router.post("/new",async(req,res)=> {
     try {
         const {name, ingredients, instructions, user_id} = req.body;
-
-        // const userId = await pool.query("SELECT user_name FROM users WHERE user_id =$1", [
-        //     req.user.user_id
-        // ])
-        // console.log(userId)
-        console.log(ingredients)
+        
         const newRecipe = await pool.query("INSERT INTO recipes (recipe_name, recipe_ingredients, recipe_instructions, user_id) VALUES ($1,$2,$3,$4) RETURNING *", [name, JSON.stringify(ingredients), instructions, user_id])
         res.json(newRecipe)
     } catch (err) {
