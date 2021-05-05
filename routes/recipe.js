9708f26eb6ebc50c9b5aa7fb94e7f38d4857614f
Vanessa Avila <69284928+regularvanessaperson@users.noreply.github.com>
2021-05-05 16:22:06 -0500

    updated field for ingredients from json obj to array for simplicity (#11)

--- routes/recipe.js
+++ routes/recipe.js
@@ -25,7 +25,7 @@ router.post("/new",async(req,res)=> {
     try {
         const {name, ingredients, instructions, user_id} = req.body;
 
-        const newRecipe = await pool.query("INSERT INTO recipes (recipe_name, recipe_ingredients, recipe_instructions, user_id) VALUES ($1,$2,$3,$4) RETURNING *", [name, JSON.stringify(ingredients), instructions, user_id])
+        const newRecipe = await pool.query("INSERT INTO recipes (recipe_name, recipe_ingredients, recipe_instructions, user_id) VALUES ($1,$2,$3,$4) RETURNING *", [name, ingredients, instructions, user_id])
         res.json(newRecipe.rows[0])
     } catch (err) {
         console.error(err)
@@ -54,7 +54,7 @@ router.put("/edit/:id", authorization, async(req, res)=>{
         const recipeId = req.params.id
         
         //need to change each value at req.body.(table_field)
-        const recipe = await pool.query("UPDATE recipes SET recipe_name=$1, recipe_ingredients=$2, recipe_instructions=$3, recipe_favorite=$4 WHERE recipe_id=$5 RETURNING * ", [req.body.recipe_name, JSON.stringify(req.body.recipe_ingredients), req.body.recipe_instructions, req.body.recipe_favorite, recipeId
+        const recipe = await pool.query("UPDATE recipes SET recipe_name=$1, recipe_ingredients=$2, recipe_instructions=$3, recipe_favorite=$4 WHERE recipe_id=$5 RETURNING * ", [req.body.recipe_name, req.body.recipe_ingredients, req.body.recipe_instructions, req.body.recipe_favorite, recipeId
         ])
 
         res.json(recipe.rows[0]);
